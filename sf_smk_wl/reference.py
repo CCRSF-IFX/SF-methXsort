09553ab1d5e94311cb50b366998b3b0d0ce998cd
xies4 <xies4@nih.gov>
2025-07-04 05:58:07 -0400

    Add xengsort index in reference.py

--- sf_smk_wl/reference.py
+++ sf_smk_wl/reference.py
@@ -1,2 +1,3 @@
-bbsplit_path = "/mnt/ccrsf-ifx/RefGenomes/dragen_ref/xenoxsort/bbsplit_idx_convert"
-bbsplit_idx = 1
+xengsort_index_prefix = "/mnt/ccrsf-ifx/RefGenomes/dragen_ref/xenoxsort/methxsort_xengsort_index_converted/methxsort_index_xs_convert_70B"
+# bbsplit_path = "/mnt/ccrsf-ifx/RefGenomes/dragen_ref/xenoxsort/bbsplit_idx_convert"
+# bbsplit_idx = 1
